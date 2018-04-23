a38d0c187ab24b545b7ac6b6583fa91407545cfd
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-23 18:35:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,17 +3,11 @@ class School
 
  def initialize(school)
    @school = school
+   roster = {}
   end
 
-  def roster
-    @roster = {}
-  end
+ def roster(school)
+   roster
 
-  def add_student(name, grade)
-    @roster = {
-      grade => []
-    }
-    @roster[grade] << name
-  end
 
 end
